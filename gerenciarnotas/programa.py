c4d333a372def7b07e8dcaf7ad54b6c1f25842af
Agathalimaxz1 <agathacunha45@gmail.com>
2025-11-04 09:01:52 -0300

    lista3

--- gerenciarnotas/programa.py
+++ gerenciarnotas/programa.py
@@ -1,37 +1,49 @@
 from funcoes import *
+from tabulate import tabulate
 lista_alunos = []
-While True:
+while True:
     print ('''Menu:
     1. Cadastrar alunos e notas
     2. Exibir relatório
     0. Sair
-    Escolha uma opção:''')
-    opcao = input("Escolha a opcao: ")
+    ''')
+    opcao = input("Escolha sua opçao: ")
     if opcao == "1":
-        nome = input("Digite o nome do aluno: ")
         notas = []
+        aluno = input("Digite o nome do aluno: ")
         for n in range(3):
-            nota = float(input("Digite as notas: "))
+            nota = int(input("Digite as notas: "))
             notas.append(nota)
-        aluno = {
-            "nome": nome,
-            "notas": notas
-        }
+        media = calcular_media(notas)
+        situacao = verificar_situacao(media)
+        aluno = {"aluno" : aluno, "notas" : notas, "situacao": situacao}
         lista_alunos.append(aluno)
     elif opcao == "2":
-        for aluno in lista_alunos:
-            media = calcular_media(aluno["notas"])
-            situacao = verificar_situacao(media)
-            print(f"Aluno: {aluno['nome']}, Média: {media}, Situação: {situacao}")
+        print(lista_alunos)
     elif opcao == "0":
-        print("Saindo do programa...")
+        print("Sair do programa")
         break
-        
+ 
     
+
+    # opcao = input("Escolha a opcao: ")
+    # if opcao == "1":
+    #     nome = input("Digite o nome do aluno: ")
+    #     notas = []
+    #     for n in range(3):
+    #         nota = float(input("Digite as notas: "))
+    #         notas.append(nota)
+    #     aluno = {
+    #         "nome": nome,
+    #         "notas": notas
+    #     }
+    #     lista_alunos.append(aluno)
+    # elif opcao == "2":
+    #     for aluno in lista_alunos:
+    #         media = calcular_media(aluno["notas"])
+    #         situacao = verificar_situacao(media)
+    #         print(f"Aluno: {aluno['nome']}, Média: {media}, Situação: {situacao}")
+    # elif opcao == "0":
+    #     print("Saindo do programa...")
      
 
-   
-
-    
-            
-           
